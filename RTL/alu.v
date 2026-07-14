933f45abfd1a4d183944758b2e50afebff0868cf
Vardha <vgoyal1_be23@thapar.edu>
2026-07-14 13:12:21 +0530

    Create alu.v

---
+++ RTL/alu.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+
+module alu(
+
+    input [31:0] A,
+    input [31:0] B,
+
+    input [3:0] ALUControl,
+
+    output reg [31:0] Result,
+    output Zero
+
+);
+
+always @(*) begin
+
+    case(ALUControl)
+
+        4'b0000: Result = A + B;                 // ADD
+
+        4'b0001: Result = A - B;                 // SUB
+
+        4'b0010: Result = A & B;                 // AND
+
+        4'b0011: Result = A | B;                 // OR
+
+        4'b0100: Result = A ^ B;                 // XOR
+
+        4'b0101: Result = (A < B) ? 32'd1 : 32'd0; // SLT
+
+        4'b0110: Result = A << B[4:0];           // SLL
+
+        4'b0111: Result = A >> B[4:0];           // SRL
+
+        default: Result = 32'd0;
+
+    endcase
+
+end
+
+assign Zero = (Result == 32'd0);
+
+endmodule
