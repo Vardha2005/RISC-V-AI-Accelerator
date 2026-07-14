7449717143cb69deb99aaf1688793f082badf730
Vardha <vgoyal1_be23@thapar.edu>
2026-07-14 13:39:55 +0530

    Create mux2to1.v

---
+++ RTL/mux2to1.v
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+
+module mux2to1(
+
+    input [31:0] input0,
+    input [31:0] input1,
+
+    input select,
+
+    output [31:0] out
+
+);
+
+assign out = (select) ? input1 : input0;
+
+endmodule
